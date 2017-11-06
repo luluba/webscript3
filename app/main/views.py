660883a4b1d370415f5daf0fd9471f1a3753e465
mandyliu0305 <mandyliu0305@gmail.com>
2017-11-05 22:36:33 -0500

    add twitter boostrap extension

--- app/main/views.py
+++ app/main/views.py
@@ -4,3 +4,7 @@ from . import main
 @main.route('/home')
 def home():
 	return render_template('home.html')
+
+@main.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
