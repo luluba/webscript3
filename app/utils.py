f4ef541653d99795ec5a0ef791c2e02053f129be
mandyliu0305 <mandyliu0305@gmail.com>
2017-11-18 23:57:37 -0500

    add base html

---
+++ app/utils.py
@@ -0,0 +1,23 @@
+from flask_login import UserMixin, AnonymousUserMixin
+from . import login_manager
+
+###HACK HACK, should get it from database
+mapping = {
+  "2": {
+    "username" :  "mandyliu0305@gmail.com"
+  }
+}
+
+class User(UserMixin):
+  def __init__(self, **kwargs):
+    super(User, self).__init__(**kwargs)
+	
+@login_manager.user_loader
+def load_user(user_id):
+  print(user_id)
+  if user_id in mapping:
+    user = User()
+    user.username = mapping[user_id]["username"]
+    return user
+
+  return None
